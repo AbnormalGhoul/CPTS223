78d7ecaf2e0287fdcf789d473628039d947a8022
“Itay <itay.sh.mail@gmail.com>
2025-02-21 20:02:51 -0800

    Init Update

--- Destination.cpp
+++
@@ -1,7 +0,0 @@
-#include "Destination.h"
-
-Destination::Destination(int pos, std::string cityName) : position(pos), name(cityName) {}
-Destination::~Destination() {}
-void Destination::printPosition() const { std::cout << position << std::endl; }
-void Destination::printDestinationName() const { std::cout << name << std::endl; }
-std::string Destination::getName() const { return name; }
